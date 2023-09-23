dcd9114ae5a1485fcaf9bbc2dd67348d41d12cf8
Kristiyan Stefanov <92256441+Kristiqan@users.noreply.github.com>
2023-09-23 22:24:41 +0300

    Chucknorris-api

---
+++ chucknorris-api/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {JokesService} from './jokes.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  jokes: any[] = [];
+  categories: any[] = [];
+  
+  constructor(
+    private jokesService: JokesService
+  ){}
+  
+  ngOnInit(){
+    this.jokesService.getCategories()
+    .subscribe((categories: any) =>{
+      this.categories = categories;
+ 
+      this.jokesService.getRandomJoke()
+      .subscribe(joke =>{
+        this.jokes.push(joke);
+      });
+    });
+  }
+
+searchByCategory(category: string){
+  this.jokesService.getCategoryJoke(category)
+  .subscribe(joke =>{
+    this.jokes =[];
+    this.jokes.push(joke);
+
+  });
+}
+
+searchBySearchTerm(searchTerm: string){
+ if (searchTerm !== ''){
+  this.jokesService.getSearchJokes(searchTerm)
+  .subscribe((jokes: any) => {
+  this.jokes = jokes.result;
+});
+ }
+}
+
+
+}
